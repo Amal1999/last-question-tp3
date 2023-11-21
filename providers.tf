b432f85e1fad17d42feb12a1d269100c51d68569
Amal1999 <sammari_a@outlook.fr>
2023-11-21 20:52:07 +0100

    modif providers 1

--- providers.tf
+++ providers.tf
@@ -7,6 +7,8 @@ provider "azurerm" {
   features {}
   subscription_id = "9ae5c229-9413-4920-bd6d-4b8b0ea5d15d"
   tenant_id       = "dbd6664d-4eb9-46eb-99d8-5c43ba153c61"
+  client_id       = "53dceb8c-612b-46de-a815-b51f761e167c"
+  client_secret       = "Ba58Q~FWpckS.HD5s39HGpXR.GYmA~U_mMGiTabX"
 }
 
 terraform {
