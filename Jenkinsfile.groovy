d69b6b610fd54cdc2e98b29b45d01c800b979e13
Amal1999 <sammari_a@outlook.fr>
2023-12-06 23:18:03 +0100

    just for test 2

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -44,7 +44,7 @@ pipeline {
                     withCredentials([usernamePassword(credentialsId: 'AzureServicePrincipal', passwordVariable: 'AZURE_CLIENT_SECRET', usernameVariable: 'AZURE_CLIENT_ID')]) {
                         sh 'az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID'
                         sh 'az account set -s $AZURE_SUBSCRIPTION_ID'
-                        sh 'terraform init'
+                      //  sh 'terraform init'
 
                     }
                 }
